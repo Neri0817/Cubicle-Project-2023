a20a7670888c3bd749fe33841e17f6e17f6bb51f
Neri <91215462+Neri0817@users.noreply.github.com>
2023-01-22 00:13:22 +0200

    Add a cube with persistance

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 require("./config/viewEngine")(app);
 
 app.use(express.static("src/public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
 app.listen(config.PORT, () => {

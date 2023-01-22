6bbb7af34dc2c479a535400347224527b60eee38
Neri <91215462+Neri0817@users.noreply.github.com>
2023-01-22 20:47:44 +0200

    Add details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const Cube = require("../models/Cube");
+const db = require("../db.json");
 
 exports.getCreateCube = (req, res) => {
   res.render("create");
@@ -12,4 +13,19 @@ exports.postCreateCube = (req, res) => {
 
   //redirect
   res.redirect("/");
+};
+
+exports.getDetails = (req, res) => {
+  let cubeId = Number(req.params.cubeId);
+
+  if (!cubeId) {
+    return res.redirect("404");
+  }
+  let cube = db.cubes.find((c) => c.id === cubeId);
+
+  if (!cube) {
+    return res.redirect("404");
+  }
+
+  res.render("details", { cube });
 };
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ router.get("/", homeController.getHomePage);
 router.get("/about", homeController.getAboutPage);
 router.get("/create", cubeController.getCreateCube);
 
-router.post('/create', cubeController.postCreateCube);
+router.post("/create", cubeController.postCreateCube);
+router.get("/details/:cubeId", cubeController.getDetails);
 
 module.exports = router;

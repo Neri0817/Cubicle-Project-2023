b91ebe46c51da5ecd536a128965a23f989dd7bd6
Neri <91215462+Neri0817@users.noreply.github.com>
2023-01-29 15:44:45 +0200

    Save cube to db

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,15 +1,16 @@
-const Cube = require("../models/Cube_old");
+// const Cube = require("../models/Cube_old");
+const Cube = require("../models/Cube");
 const db = require("../db.json");
 
 exports.getCreateCube = (req, res) => {
   res.render("create");
 };
 
-exports.postCreateCube = (req, res) => {
+exports.postCreateCube = async (req, res) => {
   const { name, description, imageUrl, difficultyLevel } = req.body;
   //save cube
-  let cube = new Cube(name, description, imageUrl, difficultyLevel);
-  cube.save();
+  let cube = new Cube({ name, description, imageUrl, difficultyLevel });
+  await cube.save();
 
   //redirect
   res.redirect("/");

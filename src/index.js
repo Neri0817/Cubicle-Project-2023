1c7842c7205265a0a414f69df1826a81f04cfdb7
Neri <91215462+Neri0817@users.noreply.github.com>
2023-01-29 15:34:48 +0200

    Add mongoose
    Add cube model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const config = require("./config");
+const initDatabase = require("./config/initDatabase");
 
 //* const setupViewEngine = require("./setupViewEngine");
 //* setupViewEngine(app);
@@ -11,6 +12,10 @@ app.use(express.static("src/public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(config.PORT, () => {
-  console.log("Server is running on port " + config.PORT);
-});
+initDatabase()
+  .then(() =>
+    app.listen(config.PORT, () => {
+      console.log("Server is running on port " + config.PORT);
+    })
+  )
+  .catch((err) => console.error(err.message));
